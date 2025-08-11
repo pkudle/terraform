aff87b53f76119c04506424815bf088c3d6a0ba3
Pratik Kudle <pkudle@gmail.com>
2025-08-11 12:54:41 +0530

    Sample ec2.tf file

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-south-1a"
+}
+
+resource "aws_instance" "my-instance-0" {
+    ami = "ami-0f918f7e67a3323f0"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-015c7634dc8c2fa00"]
+}
